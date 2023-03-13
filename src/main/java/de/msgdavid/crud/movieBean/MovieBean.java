6e065e0092bd7bd460344f5c0447d16d5f22be9e
Mansur Ismailov <mansur.ismailov@msg.group>
2023-03-13 10:34:33 +0100

    Adding JSF Project

---
+++ src/main/java/de/msgdavid/crud/movieBean/MovieBean.java
@@ -0,0 +1,38 @@
+package de.msgdavid.crud.movieBean;
+
+import de.msgdavid.crud.movie.Movie;
+import de.msgdavid.crud.databse.operation.DatabaseOperation;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.util.ArrayList;
+
+@ManagedBean(name = "movie")
+@RequestScoped
+public class MovieBean extends Movie {
+
+    @PostConstruct
+    public void init() {
+        movieListFromDB = DatabaseOperation.getMovieListFromDB();
+    }
+    public ArrayList<MovieBean> movieList() {
+        return movieListFromDB;
+    }
+
+    public String saveMovieDetails(MovieBean newMovieObject) {
+        return DatabaseOperation.saveMovieDetailInDB(newMovieObject);
+    }
+
+    public String editMovieRecord(int movieID) {
+        return DatabaseOperation.editMovieRecordInDB(movieID);
+    }
+
+    public String updateMovieDetails(MovieBean updateMovieObject) {
+        return DatabaseOperation.updateMovieDetailInDB(updateMovieObject);
+    }
+
+    public String deleteMovieRecord(int movieID) {
+        return DatabaseOperation.deleteMovieRecordInDB(movieID);
+    }
+}
