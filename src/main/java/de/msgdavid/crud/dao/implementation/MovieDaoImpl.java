c40efd7d4becc96d1f7fee5c645f4e40897e4a57
Mansur Ismailov <mansur.ismailov@msg.group>
2023-06-14 12:10:47 +0200

    Updating methods

--- src/main/java/de/msgdavid/crud/dao/implementation/MovieDaoImpl.java
+++ src/main/java/de/msgdavid/crud/dao/implementation/MovieDaoImpl.java
@@ -1,13 +1,14 @@
 package de.msgdavid.crud.dao.implementation;
 
+import de.msgdavid.crud.dao.connection.ConnectionFactory;
 import de.msgdavid.crud.dao.interf.IMovieDao;
 import de.msgdavid.crud.entity.Movie;
 
+import javax.faces.context.FacesContext;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import static de.msgdavid.crud.database.operation.DatabaseConnection.getConnection;
+import java.util.Map;
 
 public class MovieDaoImpl implements IMovieDao {
     private  Statement statementObject;
@@ -19,7 +20,7 @@ public class MovieDaoImpl implements IMovieDao {
     public List<Movie> readAll() {
         List<Movie> movieList = new ArrayList<>();
         try {
-            connectionObject = getConnection();
+            connectionObject = ConnectionFactory.getInstance().getConnection();
             statementObject = connectionObject.createStatement();
             resultSetObject = statementObject.executeQuery("select * from movielist");
             while (resultSetObject.next()) {
@@ -34,6 +35,91 @@ public class MovieDaoImpl implements IMovieDao {
         }
         return movieList;
     }
+
+    @Override
+    public String addMovie(Movie movie) {
+        int saveResult = 0;
+        String navigationResult;
+        try {
+            connectionObject = ConnectionFactory.getInstance().getConnection();
+            preparedStatementObject = connectionObject.prepareStatement("insert into movielist (MovieTitle, " +
+                    "Genre, MovieLength, ImdbRating, ReleaseYear) values (?, ?, ?, ?, ?)");
+            preparedStatementObject.setString(1, movie.getMovieTitle());
+            preparedStatementObject.setString(2, movie.getGenre());
+            preparedStatementObject.setString(3, movie.getMovieLength());
+            preparedStatementObject.setDouble(4, movie.getImdbRating());
+            preparedStatementObject.setInt(5, movie.getReleaseYear());
+            saveResult = preparedStatementObject.executeUpdate();
+            connectionObject.close();
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        }
+        if(saveResult !=0) {
+            navigationResult = "movieList.xhtml?faces-redirect=true";
+        } else {
+            navigationResult = "createMovie.xhtml?faces-redirect=true";
+        }
+        return navigationResult;
+    }
+
+    @Override
+    public String getMovie(int movie) {
+        Movie editRecord = null;
+        System.out.println("editMovieRecordInDB() : Movie ID: " + movie);
+
+        Map<String,Object> sessionMapObj = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+
+        try {
+            connectionObject = ConnectionFactory.getInstance().getConnection();
+            statementObject = connectionObject.createStatement();
+            resultSetObject = statementObject.executeQuery("select * from movielist where ID = "+ movie);
+            if(resultSetObject != null) {
+                resultSetObject.next();
+                editRecord = new Movie();
+                SetMovieObject(editRecord);
+            }
+            sessionMapObj.put("editMovieObject", editRecord);
+            connectionObject.close();
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        }
+        return "/editMovies.xhtml?faces-redirect=true";
+    }
+
+    @Override
+    public String updateMovie(Movie movie) {
+        try {
+            connectionObject = ConnectionFactory.getInstance().getConnection();
+            preparedStatementObject = connectionObject.prepareStatement("update movielist set MovieTitle=?, " +
+                    "Genre=?, MovieLength=?, ImdbRating=?, ReleaseYear=? where ID=?");
+            preparedStatementObject.setString(1,movie.getMovieTitle());
+            preparedStatementObject.setString(2,movie.getGenre());
+            preparedStatementObject.setString(3,movie.getMovieLength());
+            preparedStatementObject.setDouble(4,movie.getImdbRating());
+            preparedStatementObject.setInt(5,movie.getReleaseYear());
+            preparedStatementObject.setInt(6,movie.getMovieID());
+            preparedStatementObject.executeUpdate();
+            connectionObject.close();
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        }
+        return "/movieList.xhtml?faces-redirect=true";
+    }
+
+    @Override
+    public String deleteMovie(int movieID) {
+        System.out.println("deleteMovieRecord() : Student ID: " + movieID);
+        try {
+            connectionObject = ConnectionFactory.getInstance().getConnection();
+            preparedStatementObject = connectionObject.prepareStatement("delete from movielist where ID = "+movieID);
+            preparedStatementObject.executeUpdate();
+            connectionObject.close();
+        } catch(Exception sqlException){
+            sqlException.printStackTrace();
+        }
+        return "/movieList.xhtml?faces-redirect=true";
+    }
+
 
     private void SetMovieObject(Movie movieObject) throws SQLException {
         movieObject.setMovieID(resultSetObject.getInt("ID"));
--- src/main/java/de/msgdavid/crud/dao/interf/IMovieDao.java
+++ src/main/java/de/msgdavid/crud/dao/interf/IMovieDao.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public interface IMovieDao {
     List<Movie> readAll();
-    //void addMovie(Movie movie);
-    //void updateMovie(Movie movie);
-    //void deleteMovie(Movie movie);
+    String addMovie(Movie movie);
+    String getMovie(int movie);
+    String updateMovie(Movie movie);
+    String deleteMovie(int movieID);
 }
